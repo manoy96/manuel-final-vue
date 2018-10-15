2ec64b58d89d90ab2157337c6fa4c97cb1eb42ff
Manuel Espinoza <mfewcar@gmail.com>
2018-10-15 15:44:47 -0600

    started playing around with SWapi

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <ul>
+      <li v-for="film in allFilms" :key="film.episode_id">
+        {{ film.title }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+import { films } from "../assets/films";
+export default {
+  data: function(){
+    return {
+      allFilms: films
+    }   
+  },
+}
+</script>
+
+<style scoped>
+ul{
+  list-style-type: none;
+}
+
+</style>
+
+
